06d9f8643c4882736123251588f8b39f0c490018
Anbang Xu <anbangx@gmail.com>
2014-02-10 23:43:37 -0800

    Add preorder method to do serialize tree

--- src/GeekForGeek/Interview/SerializeDeserializeTree.java
+++ src/GeekForGeek/Interview/SerializeDeserializeTree.java
@@ -49,10 +49,53 @@ public class SerializeDeserializeTree {
 	    
 	    return root;
 	}
-
+	
+	public void preorderWrite(TreeNode root, StringBuilder sb){
+		if(root == null)
+			sb.append("#");
+		else{
+			sb.append(root.val);
+			preorderWrite(root.left, sb);
+			preorderWrite(root.right, sb);
+		}
+	}
+	
+	int idx = 0;
+	public TreeNode preorderRead(TreeNode root, String str){
+		if(idx == str.length())
+			return root;
+		char c = str.charAt(idx);
+		idx++;
+		if(c != '#'){
+			root = new TreeNode(c - '0');
+			preorderRead(root.left, str);
+			preorderRead(root.right, str);
+		}
+		return root;
+	}
+	
 	public static void main(String[] args) {
 		SerializeDeserializeTree sdt = new SerializeDeserializeTree();
-		sdt.deserialize("12345#6");
+//		sdt.deserialize("12345#6");
+		
+		TreeNode l1 = new TreeNode(1);
+		TreeNode l21 = new TreeNode(2);
+		TreeNode l22 = new TreeNode(3);
+		l1.left = l21; l1.right = l22;
+		TreeNode l31 = new TreeNode(4);
+		l21.left = l31;
+		TreeNode l33 = new TreeNode(5);
+		TreeNode l34 = new TreeNode(6);
+		l22.left = l33;
+		l22.right = l34;
+		StringBuilder sb = new StringBuilder();
+		sdt.preorderWrite(l1, sb);
+		String s = sb.toString();
+		System.out.println(s);
+		
+		TreeNode root = sdt.preorderRead(null, s);
+		
+		System.out.println(root);
 	}
 
 }
